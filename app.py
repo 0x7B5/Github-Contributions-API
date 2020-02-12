0b9d304e4cbd2f832ede7fd0e1b0287b137402a0
Vlad Munteanu <vladcmunteanu@gmail.com>
2020-02-12 13:28:52 -0500

    Attribute Parsing

--- app.py
+++ app.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python3
 import requests
+from datetime import datetime
 from bs4 import BeautifulSoup
 
 baseurl = "https://github.com/vlad-munteanu?tab=overview&from=2020-01-01&to=2020-12-31"
 username = "vlad-munteanu"
 
 page = requests.get(baseurl)
-print(type(page))
 #print(page.content)
 soup = BeautifulSoup(page.content, 'html.parser')
 
-day_elems = soup.find_all(class_='day')
+day_elems = soup.find_all('rect')
 
 for day_elem in day_elems:
-    print(day_elem, end='\n')
+    tempDate = datetime.strptime(day_elem.attrs['data-date'], '%Y-%m-%d')
+    if (tempDate <= datetime.today()):
+        print("Date:", day_elem.attrs['data-date'], "Fill:", day_elem.attrs['fill'], "Contributions:", day_elem.attrs['data-count'])
+    else:
+        break
+
+currentYear = datetime.now().year 
+print(currentYear)
