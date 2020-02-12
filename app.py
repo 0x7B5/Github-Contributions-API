80d05c395a08c41b39fccd9e18ce86e0e002614d
Vlad Munteanu <vladcmunteanu@gmail.com>
2020-02-12 16:32:13 -0500

    Got contribution data showing in json data.

--- app.py
+++ app.py
@@ -1,23 +1,46 @@
 #!/usr/bin/env python3
 import requests
+from flask import Flask, jsonify
 from datetime import datetime
 from bs4 import BeautifulSoup
 
-baseurl = "https://github.com/vlad-munteanu?tab=overview&from=2020-01-01&to=2020-12-31"
-username = "vlad-munteanu"
 
-page = requests.get(baseurl)
-#print(page.content)
-soup = BeautifulSoup(page.content, 'html.parser')
+app = Flask(__name__, static_url_path="")
 
-day_elems = soup.find_all('rect')
+@app.route('/contributions/<username>', methods=['GET'])
+def getAllCommits(username):
+#    username = request.args.get('username')
+    baseurl = "https://github.com/"
+    otherUrl = "?tab=overview&from={}-01-01&to={}-12-31".format(datetime.today().year,datetime.today().year)
+    
+    
+    page = requests.get(baseurl + username + otherUrl)
+    #print(page.content)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    day_elems = soup.find_all('rect')
+    
+    contributions = [ ]
+    
+    for day_elem in day_elems:
+        tempDate = datetime.strptime(day_elem.attrs['data-date'], '%Y-%m-%d')
+        if (tempDate <= datetime.today()):
+#            print("Date:", day_elem.attrs['data-date'], "Fill:", day_elem.attrs['fill'], "Contributions:", day_elem.attrs['data-count'])
+            contributions.append({
+                                    'Date': day_elem.attrs['data-date'],
+                                    'Fill': day_elem.attrs['fill'],
+                                    'Contributions': day_elem.attrs['data-count']
+                                 })
+        else:
+            break
+    
+    return jsonify({'Data': contributions})
+  
+  
+@app.route('/DailyContributions/<username>', methods=['GET'])
+def getDailyCommit():
+    return 1
 
-for day_elem in day_elems:
-    tempDate = datetime.strptime(day_elem.attrs['data-date'], '%Y-%m-%d')
-    if (tempDate <= datetime.today()):
-        print("Date:", day_elem.attrs['data-date'], "Fill:", day_elem.attrs['fill'], "Contributions:", day_elem.attrs['data-count'])
-    else:
-        break
+if __name__ == '__main__':
+    app.run(debug=True)
 
-currentYear = datetime.now().year 
-print(currentYear)
+
