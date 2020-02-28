1f01902da56e132ae03ca42e86963235bc4383c2
Vlad Munteanu <vladcmunteanu@gmail.com>
2020-02-27 20:40:12 -0500

    Fixed json parsing to be more compatiable for iOS parsing.

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ from bs4 import BeautifulSoup
 app = Flask(__name__, static_url_path="")
 currentYear = datetime.today().year
 baseurl = "https://github.com/"
-otherUrl = "?tab=overview&from={}-01-01&to={}-12-31".format(currentYear,currentYear)
 
 @app.route('/contributions/<username>', methods=['GET'])
 def getAllCommits(username):
@@ -18,43 +17,54 @@ def getAllCommits(username):
     
     if isValidUser(username) == False:
         return jsonify("User does not exist")
-    getUserCreationYear(username)
-    page = requests.get(baseurl + username + otherUrl)
-    
-    soup = BeautifulSoup(page.content, 'html.parser')
-    day_elems = soup.find_all('rect')
-    
+    creationYear = getUserCreationYear(username)
     contributions = [ ]
     
-    for day_elem in day_elems:
-        tempDate = datetime.strptime(day_elem.attrs['data-date'], '%Y-%m-%d')
-        if (tempDate <= datetime.today()):
-            contributions.append({
-                                    'Date': day_elem.attrs['data-date'],
-                                    'Fill': day_elem.attrs['fill'],
-                                    'Contributions': day_elem.attrs['data-count']
-                                 })
-        else:
-            break
-    return jsonify({'Data': contributions})
+    for i in range(getUserCreationYear(username), currentYear+1, 1):
+        otherUrl = "?tab=overview&from={}-01-01&to={}-12-31".format(i,i)
+        page = requests.get(baseurl + username + otherUrl)
+    
+        soup = BeautifulSoup(page.content, 'html.parser')
+        day_elems = soup.find_all('rect')
+    
+        for day_elem in day_elems:
+            tempDate = datetime.strptime(day_elem.attrs['data-date'], '%Y-%m-%d')
+            if (tempDate <= datetime.today()):
+                contributions.append({
+                                    'date': day_elem.attrs['data-date'],
+                                    'color': day_elem.attrs['fill'],
+                                    'count': int(day_elem.attrs['data-count']),
+                                    'dayOfWeek': getWeekDay(day_elem.attrs['data-date'])
+                                    })
+            else:
+                break
+    return jsonify({'contributions': contributions})
   
+  
+def getWeekDay(dateToAnalyze):
+    curDate = datetime.strptime(dateToAnalyze, '%Y-%m-%d').date()
+    return curDate.weekday()
   
 @app.route('/DailyContributions/<username>', methods=['GET'])
 def getDailyCommit():
     return 1
 
 def getUserCreationYear(username):
+    """
+    So theres an api limit on this so we need to manually parse
+    """
     r = requests.get("https://api.github.com/users/{}".format(username))
     data = r.json()
-    return data['created_at'].split("-")[0]
+    return int(data['created_at'].split("-")[0])
     
 def isValidUser(username):
-    r = requests.get("https://api.github.com/users/{}".format(username))
-    data = r.json()
+ ## There's an API limit that I'm hitting for some reason
+ ## SO essentially, we need to manually parse the github site to see if they are
+ ## a valid user or not because the api is being weird
+    x = requests.get("https://github.com/".format(username))
     
-    if data['message'] == "Not Found":
-        return False
-    return True
+    print(x.status_code)
+    return True 
     
 if __name__ == '__main__':
     app.run(debug=True)
